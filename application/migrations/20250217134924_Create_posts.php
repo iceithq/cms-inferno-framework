4a1484542fb64562c06227ac261066a9d0dcafbf
Ian <ian.escarro@gmail.com>
2025-02-17 22:08:23 +0800

    init migrations

---
+++ application/migrations/20250217134924_Create_posts.php
@@ -0,0 +1,51 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Migration_Create_posts extends CI_Migration
+{
+	function up()
+  {
+		$this->dbforge->add_field(array(
+			'id' => array(
+				'type' => 'INT',
+				'constraint' => 5,
+				'unsigned' => TRUE,
+				'auto_increment' => TRUE
+			),
+			'title' => array(
+				'type' => 'varchar',
+				'constraint' => 255,
+				'null' => TRUE,
+			),
+			'content' => array(
+				'type' => 'text',
+				'null' => TRUE,
+			),
+			'created_at' => array(
+				'type' => 'datetime',
+				'null' => TRUE,
+			),
+			'updated_at' => array(
+				'type' => 'datetime',
+				'null' => TRUE,
+			),
+			'teaser' => array(
+				'type' => 'mediumtext',
+				'null' => TRUE,
+			),
+			'is_featured' => array(
+				'type' => 'integer',
+				'null' => TRUE,
+			),
+
+		));
+		$this->dbforge->add_key('id', TRUE);
+		$this->dbforge->create_table('posts');
+	}
+
+	function down()
+	{
+		$this->dbforge->drop_table('posts');
+	}
+}
